44cd7ceedfab6f8d0d0c7cb9cc65b1eb32f40c57
Peter Ukena <peter.ukena@brille24.de>
2019-04-02 11:50:00 +0200

    Added MailchimpException

---
+++ src/MailchimpException.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @author Peter Ukena <peter.ukena@brille24.de>
+ */
+
+declare(strict_types=1);
+
+namespace Brille24\Mailchimp;
+
+use Exception;
+
+class MailchimpException extends Exception
+{
+
+}
